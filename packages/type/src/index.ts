0f40faec5a01b206aecbe54179546b1ccec8c336
Pablo Rodrigo D'angelo <pablo.dangelo@famar.com>
2023-01-09 19:15:52 -0300

    Retrieve Inquirer

--- packages/type/src/index.ts
+++
@@ -1,7 +0,0 @@
-export type Context = {
-  input?: NodeJS.ReadableStream;
-  output?: NodeJS.WritableStream;
-  clearPromptOnDone?: boolean;
-};
-
-export type Prompt<Value, Config> = (config: Config, context?: Context) => Promise<Value>;
